7b5741d2141dbfac72787774ae7ac7e46bc92724
Renjie Xu <renjie.xu@chime.com>
2021-04-19 22:37:55 -0700

    Fix inherit_from not parsed error

--- lib/rubocop/extension/generator/generator.rb
+++ lib/rubocop/extension/generator/generator.rb
@@ -35,7 +35,7 @@ module RuboCop
                 module Inject
                   def self.defaults!
                     path = CONFIG_DEFAULT.to_s
-                    hash = ConfigLoader.send(:load_yaml_configuration, path)
+                    hash = ConfigLoader.send(:load_file, File.new(path))
                     config = Config.new(hash, path).tap(&:make_excludes_absolute)
                     puts "configuration from \#{path}" if ConfigLoader.debug?
                     config = ConfigLoader.merge_with_default(config, path)
